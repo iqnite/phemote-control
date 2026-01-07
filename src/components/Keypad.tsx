83bffa657e939669f798948c010ec1d70c59cad0
Benjamin Hartmann <benjamin.p.hartmann@gmail.com>
2026-01-07 10:33:31 +0100

    Fix linter issues

--- src/components/Keypad.tsx
+++ src/components/Keypad.tsx
@@ -2,26 +2,24 @@ import React from "react";
 import { IonButton } from "@ionic/react";
 import styles from "./Keypad.module.css";
 
-interface ContainerProps {}
-
-const Keypad: React.FC<ContainerProps> = () => {
-    const keys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#"];
-    return (
-        <div className={styles.bottomContainer}>
-            <div className={styles.keypadContainer}>
-                {keys.map((key) => (
-                    <IonButton
-                        key={key}
-                        color={"light"}
-                        size="large"
-                        className={styles.key}
-                    >
-                        {key}
-                    </IonButton>
-                ))}
-            </div>
-        </div>
-    );
+const Keypad: React.FC = () => {
+  const keys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#"];
+  return (
+    <div className={styles.bottomContainer}>
+      <div className={styles.keypadContainer}>
+        {keys.map((key) => (
+          <IonButton
+            key={key}
+            color={"light"}
+            size="large"
+            className={styles.key}
+          >
+            {key}
+          </IonButton>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Keypad;
