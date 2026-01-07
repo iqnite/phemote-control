343352672403dc420a112560a56c25094730acfa
iqnite <iqnitetechnologies@gmail.com>
2026-01-07 16:18:39 +0100

    Fix sounds, add input display

--- src/components/Keypad.tsx
+++ src/components/Keypad.tsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IonButton } from "@ionic/react";
 import styles from "./Keypad.module.css";
 
 const Keypad: React.FC = () => {
-    const keys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#"];
+    const maxDigits = 20;
+    const [keys] = useState<string[]>([
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "*",
+        "0",
+        "#",
+    ]);
+    const [audio, setAudio] = useState<
+        { key: string; sound: HTMLAudioElement }[]
+    >([]);
+    const [inputDisplay, setInputDisplay] = useState("");
+
+    useEffect(() => {
+        keys.forEach((key) => {
+            let soundName;
+            if (key === "*") soundName = "asterisk";
+            else if (key === "#") soundName = "hash";
+            else soundName = key;
+            const sound = new Audio(`/keypad_sfx/${soundName}.mp3`);
+            sound.loop = true;
+            setAudio((prevAudio) => [...prevAudio, { key: key, sound: sound }]);
+        });
+    }, [keys]);
+
     return (
         <div className={styles.bottomContainer}>
+            <p className={styles.inputDisplay}>{inputDisplay}</p>
             <div className={styles.keypadContainer}>
                 {keys.map((key) => (
                     <IonButton
@@ -14,7 +46,28 @@ const Keypad: React.FC = () => {
                         size="large"
                         className={styles.key}
                         onClick={() => {
-                            playKeySound(key);
+                            setInputDisplay((inputDisplay + key).slice(-maxDigits));
+                        }}
+                        onTouchStart={() => {
+                            const keySound = audio.find(
+                                (item) => item.key === key
+                            );
+                            if (keySound) {
+                                keySound.sound.currentTime = 0;
+                                setTimeout(() => {
+                                    keySound.sound.play();
+                                }, 50);
+                            }
+                            console.log("Touch start on key:", key);
+                        }}
+                        onTouchEnd={() => {
+                            const keySound = audio.find(
+                                (item) => item.key === key
+                            );
+                            if (keySound) {
+                                keySound.sound.pause();
+                                keySound.sound.currentTime = 0;
+                            }
                         }}
                     >
                         {key}
@@ -26,12 +79,3 @@ const Keypad: React.FC = () => {
 };
 
 export default Keypad;
-
-function playKeySound(key: string) {
-    let soundName;
-    if (key === "*") soundName = "asterisk";
-    else if (key === "#") soundName = "hash";
-    else soundName = key;
-    const sound = new Audio(`/keypad_sfx/${soundName}.mp3`);
-    sound.play();
-}
