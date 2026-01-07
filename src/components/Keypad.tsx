f42523ecc6f717b3b3dc9939083f35d4943ba96c
ZXMushroom63 <zxmushroom63@gmail.com>
2026-01-07 21:02:38 +0100

    add keyboard support

--- src/components/Keypad.tsx
+++ src/components/Keypad.tsx
@@ -1,91 +1,101 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { IonButton } from "@ionic/react";
 import styles from "./Keypad.module.css";
 
 const Keypad: React.FC = () => {
-    const maxDigits = 20;
-    const [keys] = useState<string[]>([
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "*",
-        "0",
-        "#",
-    ]);
-    const [audio, setAudio] = useState<
-        { key: string; sound: HTMLAudioElement }[]
-    >([]);
-    const [inputDisplay, setInputDisplay] = useState("");
+  const maxDigits = 20;
+  const buttonRef = useRef<HTMLIonButtonElement>(null);
+  const [keys] = useState<string[]>([
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "*",
+    "0",
+    "#",
+  ]);
+  const [audio, setAudio] = useState<
+    { key: string; sound: HTMLAudioElement }[]
+  >([]);
+  const [inputDisplay, setInputDisplay] = useState("");
 
-    useEffect(() => {
-        keys.forEach((key) => {
-            let soundName = key.replace("*", "asterisk").replace("#", "hash");
+  useEffect(() => {
+    keys.forEach((key) => {
+      let soundName = key.replace("*", "asterisk").replace("#", "hash");
 
-            const sound = new Audio(`/keypad_sfx/${soundName}.mp3`);
-            sound.load();
-            sound.loop = false;
-            setAudio((prevAudio) => [...prevAudio, { key: key, sound: sound }]);
-        });
-    }, [keys]);
+      const sound = new Audio(`/keypad_sfx/${soundName}.mp3`);
+      sound.load();
+      sound.loop = false;
+      setAudio((prevAudio) => [...prevAudio, { key: key, sound: sound }]);
+    });
+  }, [keys]);
 
-    return (
-        <div className={styles.bottomContainer}>
-            <p className={styles.inputDisplay} onMouseDown={()=>setInputDisplay("")}>
-              {inputDisplay}
-            </p>
-            <div className={styles.keypadContainer}>
-                {keys.map((key) => {
-                    function activateKeypad(event: any, keycode: string) {
-                        const keySound = audio.find(
-                            (item) => item.key === key
-                        );
-                        if (event instanceof TouchEvent) {
-                          if (event.touches.length > 0) {return;}
-                        }
-                        if (keycode && (keycode !== "Enter")) {
-                          return;
-                        }
-                        if (keySound) {
-                            keySound.sound.volume = 1;
-                            keySound.sound.currentTime = 0;
-                            setTimeout(()=>keySound.sound.play(), 0);
-                        }
-                        setInputDisplay((inputDisplay + key).slice(-maxDigits));
-                    }
+  return (
+    <div className={styles.bottomContainer}>
+      <p className={styles.inputDisplay} onMouseDown={() => setInputDisplay("")}>
+        {inputDisplay}
+      </p>
+      <div className={styles.keypadContainer}>
+        {keys.map((key) => {
+          function activateKeypad(event: any, keycode: string) {
+            const keySound = audio.find(
+              (item) => item.key === key
+            );
+            if (event instanceof TouchEvent) {
+              if (event.touches.length > 0) { return; }
+            }
+            if (keycode && (keycode !== "Enter")) {
+              return;
+            }
+            if (keySound) {
+              keySound.sound.volume = 1;
+              keySound.sound.currentTime = 0;
+              setTimeout(() => keySound.sound.play(), 0);
+            }
+            setInputDisplay((inputDisplay + key).slice(-maxDigits));
+          }
 
-                    function stopAudioTrack() {
-                        const keySound = audio.find(
-                            (item) => item.key === key
-                        );
-                        if (keySound && !keySound.sound.paused) {
-                            keySound.sound.volume = 0;
-                        }
-                    }
-                    
-                    return <IonButton
-                        key={key}
-                        size="large"
-                        color="light"
-                        className={styles.key + (isFinite(+key) ? " " +styles.numberKey : "")}
-                        
-                        onMouseDown={e=>activateKeypad(e, "")}
-                        onKeyDown={e=>activateKeypad(e, e.repeat ? "ignore" : e.key)}
-                        //onMouseUp={stopAudioTrack}
-                        onTouchStart={e=>activateKeypad(e, "")}
-                        //onTouchCancel={stopAudioTrack}
-                    >
-                        {key}
-                    </IonButton>
-})}
-            </div>
-        </div>
-    );
+          function stopAudioTrack() {
+            const keySound = audio.find(
+              (item) => item.key === key
+            );
+            if (keySound && !keySound.sound.paused) {
+              keySound.sound.volume = 0;
+            }
+          }
+
+          const button = <IonButton
+            ref={buttonRef}
+            key={key}
+            size="large"
+            color="light"
+            className={styles.key + (isFinite(+key) ? " " + styles.numberKey : "")}
+
+            onMouseDown={e => activateKeypad(e, "")}
+            onKeyDown={e => activateKeypad(e, e.repeat ? "ignore" : e.key)}
+            //onMouseUp={stopAudioTrack}
+            onTouchStart={e => activateKeypad(e, "")}
+          //onTouchCancel={stopAudioTrack}
+          >
+            {key}
+          </IonButton>;
+
+          window.addEventListener("keydown", (e) => {
+            if ((e.key === key) && buttonRef.current && !e.repeat) {
+              setTimeout(()=>activateKeypad(e, ""), 0);
+            }
+          });
+
+          return button;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Keypad;
